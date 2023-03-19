ffc6d8b229b37fb9409a57b0a0bdf83998799bf1
Okami-Gaijin <78839188+Okami-Gaijin@users.noreply.github.com>
2023-03-18 17:38:29 -0700

    wrote first instance of Mav, can say hi, endpoints need work

---
+++ mav_main.py
@@ -0,0 +1,59 @@
+import discord
+import requests
+from graphqlclient import GraphQLClient
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+
+graphql_client = GraphQLClient('https://www.dnd5eapi.co/graphql')
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hi'):
+        await message.channel.send('Salutations, young one!')
+
+    if message.content.startswith('!charisma'):
+        # Define the query (fixed missing quote)
+        query = """
+        {
+            abilityScore(name: "Charisma") {
+                name
+                desc
+            }
+        }
+        """
+        # Executes the query and gets response
+        result = graphql_client.execute(query)
+
+        # Sends response as a message in Discord
+        await message.channel.send(result)
+
+    elif message.content.startswith('!beholder'):
+        # Define the API endpoint and the query parameter
+        endpoint = 'https://api.open5e.com/monsters/'
+        params = {'name': 'Beholder'}
+
+        # Make a GET request to the API
+        response = requests.get(endpoint, params=params)
+
+        # Check if response is successful (status code 200)
+        if response.status_code == 200:
+            # Send the response content as a message in Discord
+            await message.channel.send(response.json())
+        else:
+            # Handle errors by sending a message in Discord
+            await message.channel.send(f"Request failed with status code {response.status_code}")
+            
+
+client.run('MTA4Njc4MDU5NTUwMzM3NDQzOA.Gb6cZE.0G4HeRkoAqM4KtePjb89ig4SYpmgugK12vRGBA')
